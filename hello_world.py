619f1b12d5853583cab4428ccb5d931716550065
monterey56 <wowpunkwar@gmail.com>
2023-07-20 15:10:38 +0100

    initial

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+#Ask user for their name and age and print them each on a new line.
+name = input("Enter your name: ")
+print(name)
+age = input("Enter your age: ")
+print(age)
+#print hello world
+print("Hello World!")
---
+++ menu.py
@@ -0,0 +1,9 @@
+#Ask user for their 3 favourite foods and print them out in a list format.
+print("What is your favourite food?")
+item1 = input("Enter your first item: ")
+item2 = input("Enter your second item: ")
+item3 = input("Enter your third item: ")
+print("Order confirmation! You have ordered:")
+print(item1)
+print(item2)
+print(item3)
